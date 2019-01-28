037185fe6393b5166e24dafdf2917f1a300c0f39
iamsitting <cdsalamanca27@gmail.com>
2019-01-28 07:49:12 -0800

    rm comments

--- website/frontend/src/app/contact-us/contact-us.component.ts
+++ website/frontend/src/app/contact-us/contact-us.component.ts
@@ -18,20 +18,6 @@ export class ContactUsComponent implements OnInit {
     exbutton.classList.remove('show');
     Button.classList.add('show');
     Button.classList.remove('hidden');
-
-    // using SendGrid's v3 Node.js Library
-    // https://github.com/sendgrid/sendgrid-nodejs
-    //const sgMail = require('@sendgrid/mail');
-    //const SENDGRID_API_KEY="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";
-    //sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    //const msg = {
-    //to: 'christiansattamu@gmail.com',
-    //from: 'christiansattamu@gmail.com',
-    //subject: 'Sending with SendGrid is Fun',
-    //text: 'and easy to do anywhere, even with Node.js',
-    //html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    //};
-    //sgMail.send(msg);
   }
 
 }
